a81a74fe4b095077311bd6f051e0e577f481f650
Damon Swatton [A10] <damon.swatton@gmail.com>
2026-04-08 10:54:45 +0100

    Initial commit.

---
+++ cmake/config.cmake
@@ -0,0 +1,5 @@
+# Use pre-defined installation directory variables.
+include(GNUInstallDirs)
+
+# Configure CMAKE_INSTALL_PREFIX for a local install.
+set(CMAKE_INSTALL_PREFIX ${PROJECT_SOURCE_DIR}/install)
